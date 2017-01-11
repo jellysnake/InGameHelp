f26fff71966ead89ca270d9c59fb76e359e9a307
Quinn <quinn@jelly-snakes.com>
2017-01-12 10:33:49 +1100

    Add dependency on new Inventory Module Fix checkstyle issue with license

--- src/main/java/org/terasology/inGameHelp/helpCategories/ItemsCategory.java
+++ src/main/java/org/terasology/inGameHelp/helpCategories/ItemsCategory.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.terasology.inGameHelp.helpCategories;
 
 import com.google.common.collect.Iterables;
